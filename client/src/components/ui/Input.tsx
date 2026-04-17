e9ac30101809bd88b2c18b6fa527435c3dcd245e
finalSay9 <chimwazaevan1@gmail.com>
2026-04-17 16:09:44 +0200

    fixing some things in the frondend/client

--- client/src/components/ui/Input.tsx
+++ client/src/components/ui/Input.tsx
@@ -5,6 +5,7 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   error?: string
 }
 
+
 export const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ label, error, className = '', ...props }, ref) => (
     <div className="flex flex-col gap-1.5">
